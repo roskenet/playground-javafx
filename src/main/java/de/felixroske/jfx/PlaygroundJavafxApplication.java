3794c29099e1fbf66663266de265955516e4229b
Felix Roske <f.roske@roskenet.de>
2015-10-09 12:55:12 +0200

    Changed Structure according to https://github.com/thomasdarimont/spring-labs

--- src/main/java/de/felixroske/jfx/PlaygroundJavafxApplication.java
+++ src/main/java/de/felixroske/jfx/PlaygroundJavafxApplication.java
@@ -1,19 +1,38 @@
 package de.felixroske.jfx;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
+import de.felixroske.jfx.main.MainView;
+import de.felixroske.jfx.support.AbstractJavaFxApplicationSupport;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+@Lazy
 @SpringBootApplication
-public class PlaygroundJavafxApplication implements CommandLineRunner{
+public class PlaygroundJavafxApplication extends AbstractJavaFxApplicationSupport{
 
-    public static void main(String[] args) {
-        SpringApplication.run(PlaygroundJavafxApplication.class, args);
-    }
+	/**
+	 * Note that this is configured in application.properties
+	 */
+	@Value("${app.ui.title:Example App}")//
+	private String windowTitle;
+
+	@Autowired MainView mainView;
 
 	@Override
-	public void run(String... arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void start(Stage stage) throws Exception {
+
+		stage.setTitle(windowTitle);
+		stage.setScene(new Scene(mainView.getView()));
+		stage.setResizable(true);
+		stage.centerOnScreen();
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launchApp(PlaygroundJavafxApplication.class, args);
 	}
 }
---
+++ src/main/java/de/felixroske/jfx/main/MainView.java
@@ -0,0 +1,9 @@
+package de.felixroske.jfx.main;
+
+import org.springframework.stereotype.Component;
+
+import de.felixroske.jfx.support.AbstractFxmlView;
+@Component
+public class MainView extends AbstractFxmlView{
+
+}
