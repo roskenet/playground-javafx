913f5a5d8edf731f11f2bfdb7a3a85a10c0804fe
Felix Roske <f.roske@roskenet.de>
2015-10-09 10:04:52 +0200

    Initial commit

---
+++ src/main/java/de/felixroske/jfx/PlaygroundJavafxApplication.java
@@ -0,0 +1,12 @@
+package de.felixroske.jfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundJavafxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygroundJavafxApplication.class, args);
+    }
+}
---
+++ src/test/java/de/felixroske/jfx/PlaygroundJavafxApplicationTests.java
@@ -0,0 +1,16 @@
+package de.felixroske.jfx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PlaygroundJavafxApplication.class)
+public class PlaygroundJavafxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
